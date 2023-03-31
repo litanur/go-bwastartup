1a5b509e499aafc2ad828ec88fb176c1ac9e294e
Lita Nurlaelati <litanurlaelati@gmail.com>
2023-04-01 06:27:20 +0700

    add route handler

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"bwastartup/user"
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 )
 
 func main() {
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:4dm1n-31@tcp(127.0.0.1:8889)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -17,18 +24,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connected to database")
-
 	var users []user.User
-	length := len(users)
-
-	fmt.Println(length)
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.Email)
-	}
+	c.JSON(http.StatusOK, users)
 }
