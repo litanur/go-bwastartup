935b2180afc9a01ae1d560185f526a20ac5a6d72
Lita Nurlaelati <litanurlaelati@gmail.com>
2023-04-01 06:17:13 +0700

    get all users

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bwastartup/user"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,11 +11,24 @@ import (
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:4dm1n-31@tcp(127.0.0.1:8889)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connected to database")
+
+	var users []user.User
+	length := len(users)
+
+	fmt.Println(length)
+	db.Find(&users)
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Email)
+	}
 }
