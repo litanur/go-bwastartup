b2e0cb0643ac2e960cb59639e72b313487a3e292
Lita Nurlaelati <litanurlaelati@gmail.com>
2023-04-02 17:08:21 +0700

    registration user

--- main.go
+++ main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"bwastartup/user"
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
-	"net/http"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:4dm1n-31@tcp(127.0.0.1:8889)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
-	var users []user.User
-	db.Find(&users)
-
-	c.JSON(http.StatusOK, users)
 }
